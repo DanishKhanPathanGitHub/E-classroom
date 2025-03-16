15828986785829af8077b92256fef5402eeddca0
Sarim Jafri <thesarimabbas@gmail.com>
2025-03-16 23:21:33 +0530

    corrected the app.jsx and some other files

--- E-classroom-frontend/src/components/UserMenu.tsx
+++ E-classroom-frontend/src/components/UserMenu.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { User, LogOut, Settings } from 'lucide-react';
-import { auth } from '../lib/firebase';
+
 import { logout } from '../store/slices/authSlice';
 import { RootState } from '../store/store';
 
